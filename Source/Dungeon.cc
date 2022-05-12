5070e70a6fea58db215ba8731d5bd112a245fbce
ganisus <ganisus08@gmail.com>
2022-05-13 08:31:04 +0900

    Fix: ビルドエラー修正(改行されないバグ発生中)

--- Source/Dungeon.cc
+++ Source/Dungeon.cc
@@ -3,27 +3,23 @@
 #include <ncurses.h>
 namespace rogue {
 Dungeon::Dungeon() {
-    m_symbol = {
-        "-----------------",
-        "|...............|",
-        "+...............|",
-        "|...............|",
-        "|...............|",
-        "-----------------"
-    };
+    m_symbol = "-----------------|¥n";
+    m_symbol += "|...............|¥n";
+    m_symbol += "+...............|¥n";
+    m_symbol += "|...............|¥n";
+    m_symbol += "|...............|¥n";
+    m_symbol += "-----------------¥n";
 }
 
+Dungeon::~Dungeon(){}
+
 void Dungeon::Print() {
-        std::int32_t row = 0;
-        std::int32_t column = 0;
-        std::int32_t scrrow = 0;
-        std::int32_t scrcolumn = 0;
-        getmaxyx(stdscr, row, column);
-        scrrow = row / 2;
-        scrcolumn = ((column - 1) / 2);
-    for(std::int32_t i = 0; i < DUNGEON_ROW_SIZE; i++) {
-        mvprintw(scrrow , scrcolumn, m_symbol[i]);
+        std::int32_t console_height = 0;
+        std::int32_t console_width = 0;
+        getmaxyx(stdscr, console_height, console_width);
+        std::int32_t dungeon_y = (console_height - 7) / 2;
+        std::int32_t dungeon_x = (console_width - 18) / 2;
+        mvprintw(dungeon_y , dungeon_x, m_symbol.c_str());
     }
-}
 }
 
