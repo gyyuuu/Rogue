9e7e4263e40791610d67530e2e9cbe9f5a4ac807
ganisus <ganisus08@gmail.com>
2022-05-14 19:01:47 +0900

    Add: ダンジョン表示機能追加(何も表示されないバグ発生中)

--- Source/Dungeon.cc
+++ Source/Dungeon.cc
@@ -1,25 +1,58 @@
 #include "Dungeon.hpp"
+#include "Position.hpp"
 
-#include <ncurses.h>
 namespace rogue {
 Dungeon::Dungeon() {
-    m_symbol = "-----------------|¥n";
-    m_symbol += "|...............|¥n";
-    m_symbol += "+...............|¥n";
-    m_symbol += "|...............|¥n";
-    m_symbol += "|...............|¥n";
-    m_symbol += "-----------------¥n";
-}
+    Position tile_position = GetCenter();
+    for (std::int32_t width = 0; width < 18; width++) {
+        m_tiles.emplace_back(tile_position.y, tile_position.x + width, '-');
+    }
+
+     m_tiles.emplace_back(tile_position.y + 1, tile_position.x, '|' );
+    for (std::int32_t width = 0; width < 18 - 1; width++) {
+        m_tiles.emplace_back(tile_position.y + 1, tile_position.x + width, '.');
+    }
+    m_tiles.emplace_back(tile_position.y + 1, tile_position.x, '|' );
+    
+    m_tiles.emplace_back(tile_position.y + 1, tile_position.x, '|' );
+    for (std::int32_t width = 0; width < 18 - 1; width++) {
+        m_tiles.emplace_back(tile_position.y + 2, tile_position.x + width, '.');
+    }
+    m_tiles.emplace_back(tile_position.y + 2, tile_position.x, '|' );
+
+     m_tiles.emplace_back(tile_position.y + 3, tile_position.x, '+' );
+    for (std::int32_t width = 0; width < 18 - 1; width++) {
+        m_tiles.emplace_back(tile_position.y + 3, tile_position.x + width, '.');
+    }
+    m_tiles.emplace_back(tile_position.y + 3, tile_position.x, '|' );
+
+     m_tiles.emplace_back(tile_position.y + 4, tile_position.x, '|' );
+    for (std::int32_t width = 0; width < 18 - 1; width++) {
+        m_tiles.emplace_back(tile_position.y + 4, tile_position.x + width, '.');
+    }
+    m_tiles.emplace_back(tile_position.y + 4, tile_position.x, '|' );
+
+     for (std::int32_t width = 0; width < 18; width++) {
+        m_tiles.emplace_back(tile_position.y, tile_position.x + width, '-');
+    }        
+};
 
 Dungeon::~Dungeon(){}
 
-void Dungeon::Print() {
-        std::int32_t console_height = 0;
-        std::int32_t console_width = 0;
-        getmaxyx(stdscr, console_height, console_width);
-        std::int32_t dungeon_y = (console_height - 7) / 2;
-        std::int32_t dungeon_x = (console_width - 18) / 2;
-        mvprintw(dungeon_y , dungeon_x, m_symbol.c_str());
+void Dungeon::Print() const noexcept {
+    for(auto tile : m_tiles) {
+        tile.Print();
     }
-}
+};
 
+Position Dungeon::GetCenter() const noexcept {
+    Position center_position = {0,0};
+    std::int32_t console_height = 0;
+    std::int32_t console_width = 0;
+    getmaxyx(stdscr, console_height, console_width);
+    center_position.y = (console_height - 7) / 2;
+    center_position.x = ((console_width - 18) / 2);
+
+    return center_position;
+};
+}
---
+++ Source/Tile.cc
@@ -0,0 +1,22 @@
+#include "Tile.hpp"
+
+#include <cstdint>
+namespace rogue {
+Tile::Tile(std::int32_t y, std::int32_t x, char symbol){
+    m_position.y = y;
+    m_position.x = x;
+    m_symbol = symbol;
+};
+
+Tile::Tile(const Tile& tile) {
+    m_position.y = tile.m_position.y;
+    m_position.x = tile.m_position.x;
+    m_symbol = tile.m_symbol;
+};
+
+Tile::~Tile(){};
+
+void Tile::Print() const noexcept {
+    mvprintw(m_position.x, m_position.y, &m_symbol);
+};
+}
